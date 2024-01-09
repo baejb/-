faf843d77897827c24c22e0302c64b00b1d964ad
baejb <bjb6478@naver.com>
2024-01-09 23:42:11 +0900

    로그인버튼 클릭 시 모달 창 생성 구현 완료

--- client/cloudclient/src/pages/LandingPage.jsx
+++ client/cloudclient/src/pages/LandingPage.jsx
@@ -1,8 +1,85 @@
+import styled from 'styled-components';
+import { TiWeatherCloudy } from "react-icons/ti";
+import { useState, useRef} from 'react';
+import KaKaoLogin from '../components/Login/KaKaoLogin';
+const GREETING = '구르미 월드에 오신 걸 환영합니다'
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    min-height: 100vh;
+`
+const Title = styled.div`
+   font-size :24px;
+   position: absolute;
+   top: 10%;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+`
+const LoginBtn = styled.button`
+    width: 180px;
+    height: 50px;
+    border-radius: 10px;
+    position: absolute;
+    bottom: 10%;
+    font-size: 20px;
+
+`
+const ModalWrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const ModalContent = styled.div`
+    background-color: white;
+    padding: 20px;
+    width: 300px;
+    height: 240px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
 const LandingPage = () => {
+    const [LoginModal, showLoginModal] = useState(false);
+    const modalBackgroud = useRef();
+
+    const handleLoginClick = () => {
+        showLoginModal(true);
+    }
+    const handleModalClose = () => {
+        showLoginModal(false);
+    }
+    const handleModalContentClick = (e) => {
+        e.stopPropagation(); // 모달 내부 클릭 이벤트가 상위로 전파되지 않도록 함
+      };
     return (
-        <div>
-            랜딩페이지
-        </div>
+        <Container >
+
+            <Title>
+                <TiWeatherCloudy/>
+                {GREETING}
+                <TiWeatherCloudy/> 
+            </Title>
+            <LoginBtn onClick={handleLoginClick}>로그인 하기</LoginBtn>
+            {LoginModal && (
+                <ModalWrapper ref={modalBackgroud} onClick={handleModalClose}>
+                    <ModalContent onClick={handleModalContentClick}>
+                        <KaKaoLogin/>
+                    </ModalContent>
+                </ModalWrapper>
+            )}
+        </Container>
     );
 };
 
