7be53299754524b67dd69245874580cfc477b669
baejb <bjb6478@naver.com>
2024-01-14 10:53:31 +0900

    fix: 아이콘 크기 수정

--- client/cloudclient/src/components/Footer.jsx
+++ client/cloudclient/src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Container = styled.div`
 `
 const Icon = styled.div`
     width: 100px;
-    height: 45px;
+    height: 40px;
     display: flex;
     justify-content: center;
     align-items: center;
