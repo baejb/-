f30f609681ed748ec57b8856266d2538cf2d3c8a
baejb <bjb6478@naver.com>
2024-02-17 13:42:29 +0900

    feat : 액세스 토큰 ,리프레시 토큰 구현

--- client/cloudclient/src/components/Login/SetToken.js
+++ client/cloudclient/src/components/Login/SetToken.js
@@ -3,27 +3,46 @@ import moment from 'moment';
 import { baseUrl } from '../../constants';
 
 async function SetToken() {
-  // HEADER에 토큰 설정
-  axios.defaults.headers.common['atk'] = localStorage.getItem('token');
-  // 만료시간이 지났을 경우, RefreshToken을 이용하여 AccessToken 재발급
-  var expiredTime = moment.utc(localStorage.getItem('atkTime'));
-  var diffTime = moment.duration(expiredTime.diff(moment()));
-  
-  if (diffTime < 10000) {
-    axios.defaults.headers.common['rtk'] = localStorage.getItem('rtk');
+  // 토큰 및 만료 시간 가져오기
+  const accessToken = localStorage.getItem('token');
+//   const accessTokenExpireTime = moment.utc(localStorage.getItem('atkTime'));
+  const accessTokenExpireTime = moment(localStorage.getItem('atkTime'));
+
+  // 현재 시간과의 차이 계산
+  const diffTime = moment.duration(accessTokenExpireTime.diff(moment()));
+ 
+
+  // 토큰 만료 10초 전에만 처리
+  if (diffTime.asSeconds() < 10) { // asSeconds() 메서드를 사용하여 초 단위로 변환
     try {
-      const res = await axios.get(`${baseUrl}/users/reissue`);
-      localStorage.setItem('atk', res.data.token[1].token);
-      localStorage.setItem('atkTime', res.data.token[1].tokenExpiresTime);
-      axios.defaults.headers.common['atk'] = localStorage.getItem('token');
-    } catch (err) {
-      // Login 페이지로 리디렉션
-       
-    
-      console.error('Token reissue failed:', err);
+      // 리프레시 토큰 가져오기
+      const refreshToken = localStorage.getItem('rtk');
+        console.log(refreshToken);
+        console.log(accessToken);
+      // 서버에 액세스 토큰 갱신 요청
+      const response = await axios.post(`${baseUrl}/users/reissue`,{}, {
+        headers: {
+          'rtk': refreshToken // 리프레시 토큰 헤더에 설정
+        }
+      });
+
+      // 새로운 액세스 토큰 및 만료 시간 저장
+     
+      const newAccessToken = response.data.result[1].token;
+      const newAccessTokenExpireTime = response.data.result[1].tokenExpiresTime;
+      localStorage.setItem('token', newAccessToken); //token === atk
+      localStorage.setItem('atkTime', newAccessTokenExpireTime);
+     
+      console.log('reissue실행');
+      // axios의 헤더에 새로운 액세스 토큰 설정
+      axios.defaults.headers.common['atk'] = newAccessToken;
+    } catch (error) {
+      // 토큰 갱신 실패 시, 로그인 페이지로 리디렉션 또는 다른 작업 수행
+      console.error('Token reissue failed:', error);
+      // 로그인 페이지로 리디렉션
     }
   }
-  
+
   return true;
 }
 
